1f185f2bc1690cf974330193734b30817351e28a
Abhinandan-patil-2307 <abhipatil0692@gmail.com>
2024-04-14 06:06:21 +0000

    ok commit

--- file.java
+++ file.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World from java and abhi"); 
+        System.out.println("Hello, World from ok  java and abhi"); 
     }
 }
